2dbf52b38d8dab3fa099d8f6c33f182da64c37c2
imedya <imedyaosandi@gmail.com>
2025-01-10 11:59:48 +0530

    parameter changes

--- appointment-management-service/config.js
+++ appointment-management-service/config.js
@@ -1,20 +1,20 @@
 // Read configuration from the environment
 
 // Datbase configruation
-const dbHost = '';
-const dbUser = '';
-const dbPassword = '';
-const dbName = '';
-const dbPort = '';
+const dbHost = process.env.dbHost;
+const dbUser = process.env.dbUser;
+const dbPassword = process.env.dbPassword;
+const dbName = process.env.dbName;
+const dbPort = process.env.dbPort;
 
 // Twilio configruation
-const twilioAccountSid = '';
-const twilioAuthToken = '';
-const twilioFromPhoneNumber = '';
+const twilioAccountSid = process.env.accountSid;
+const twilioAuthToken = process.env.authToken;
+const twilioFromPhoneNumber = process.env.number;
 
 // User Manager configruation
-const userManagerServiceUrl = '';
-const userManagerAPIkey = '';
+const userManagerServiceUrl = 'https://ac4f2f3b-fbdf-446d-80af-dc4b33adcf65-dev.e1-us-east-azure.choreoapis.dev/appointmentservice/usermgt-service/v1.0';
+const userManagerAPIkey = process.env.apiKey;
 
 module.exports = {
     dbHost,
